305aaf7f98af373e4c036f87e556e0a25da38b3c
nathaliebsl <75685745+nathaliebsl@users.noreply.github.com>
2021-01-04 11:34:17 -0300

    desenvolvimento de API

---
+++ http/api/index.js
@@ -0,0 +1,30 @@
+const http = require('http')
+const URL = require('url')
+const fs = require('fs')
+const path = require('path')
+
+const data = require('./urls.json')
+
+http.createServer((req, res) => {
+  const { name, url, del} = URL.parse(req.url, true).query
+
+  if(!name || !url)
+    return res.end(JSON.stringify(data))
+
+  if(del) {
+    data.urls = data.urls.filter(item => String(item.url) !== String(url))
+
+    return fs.writeFile(
+      path.join(__dirname, "urls.json"),
+      JSON.stringify(data, null, 2),
+      err => {
+        if(err) throw err
+
+      res.end(JSON.stringify({message: 'ok'}))
+    })
+  }
+
+  return res.end('create')
+
+
+}).listen(3000, () => console.log('API is Running'))
